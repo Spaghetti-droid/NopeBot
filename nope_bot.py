6c02118f21b970ebec55abc6d2f867d94e54f105
Grégoire <gregoire_brebner@hotmail.com>
2025-11-02 17:47:20 +0100

    Remove dev specific commands. Use logger for logs. Remove useless description.

--- nope_bot.py
+++ nope_bot.py
@@ -10,10 +10,6 @@ from discord import app_commands
 discord.utils.setup_logging()
 LOGGER = logging.getLogger(__name__)
 
-description = '''I count nopes'''
-
-MY_GUILD = discord.Object(id=0)  # replace with your guild id
-
 
 class NopeClient(discord.Client):
     # Suppress error on the User attribute being None since it fills up later
@@ -30,13 +26,9 @@ class NopeClient(discord.Client):
         # maintain its own tree instead.
         self.tree = app_commands.CommandTree(self)
 
-    # In this basic example, we just synchronize the app commands to one guild.
-    # Instead of specifying a guild to every command, we copy over our global commands instead.
-    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
     async def setup_hook(self):
-        # This copies the global commands over to your guild.
-        self.tree.copy_global_to(guild=MY_GUILD)
-        await self.tree.sync(guild=MY_GUILD)
+        # Sync global commands. Will take up to an hour until they are shown to the end-user.
+        await self.tree.sync()
 
 
 intents = discord.Intents.default()
@@ -48,8 +40,7 @@ nopes_per_guild = dict()
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
+    LOGGER.info(f'Logged in as {client.user} (ID: {client.user.id})\n------')
 
 
 @client.tree.command()
